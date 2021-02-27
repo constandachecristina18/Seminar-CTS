915e89322e942197e7d71556eabc7865e31e9bfc
constandachecristina18 <constandachecristina18@stud.ase.ro>
2021-02-27 15:05:04 +0200

    Adaugare clase Zebra si Giraffe

--- Seminar01/src/ro/ase/cts/Main.java
+++ Seminar01/src/ro/ase/cts/Main.java
@@ -1,9 +1,26 @@
 package ro.ase.cts;
 
+import clase.Animal;
+import clase.Giraffe;
+import clase.Zebra;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Zoo zoo = new Zoo();
+		
+		Animal zebra1 = new Zebra("Monica");
+		Animal zebra2 = new Zebra("Marcel");
+		Animal girafa1 = new Giraffe("Mariana");
+		Animal girafa2 = new Giraffe("Ionel", 500);
+		
+		zoo.adaugareAnimal(zebra1);
+		zoo.adaugareAnimal(zebra2);
+		zoo.adaugareAnimal(girafa1);
+		zoo.adaugareAnimal(girafa2);
+		
+		zoo.feedAnimals();
 
 	}
 
