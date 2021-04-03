0f523d8c263117319211be9b2d627a99a2fa0286
constandachecristina18 <constandachecristina18@stud.ase.ro>
2021-04-03 14:37:14 +0300

    Creare si folosire builder

---
+++ Seminar06/src/ro/ase/cts/builder/Main.java
@@ -0,0 +1,25 @@
+package ro.ase.cts.builder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Rezervare r1 = new Rezervare(12345, false, false, false, true, "Rock");
+		
+		Rezervare r2=new RezervareBuilder().setCodRezervare(15).setAreScaunErgonomic(true).build();
+		
+		Rezervare r3 = new RezervareBuilder(16).setAreBauturaInclusa(true).setAreMuzicaAmbientala(true).build();
+		
+		System.out.println(r1.toString());
+		System.out.println(r2.toString());
+		System.out.println(r3.toString());
+		
+		RezervareBuilder rb = new RezervareBuilder();
+		Rezervare r4 = rb.setCodRezervare(21).setAreBauturaInclusa(true).build();
+		
+		
+		System.out.println(r4.toString());
+	}
+
+	
+}
